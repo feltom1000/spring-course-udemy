030076ca273d476fd98712363a97a107c52b1357
Tomas Molnar <tomas.molnar@ayi.group>
2022-07-13 11:26:25 -0300

    params

--- spring-boot-web/src/main/java/com/bolsadeideas/spring/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/spring/web/app/controllers/IndexController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,36 +15,37 @@ import com.bolsadeideas.spring.web.app.models.Usuario;
 @Controller
 @RequestMapping("/app")
 public class IndexController {
-	
-	@GetMapping({"/index", "/", "/home"})
+
+	@GetMapping({ "/index", "/", "/home" })
 	public String index(Model model) {
 		model.addAttribute("titulo", "Hola a todos esto es un valor metido en la variable titulo");
 		return "index";
 	}
-	
+
 	@RequestMapping(value = "/perfil", method = RequestMethod.GET)
-		public String perfil(Model model) {
-			Usuario usuario = new Usuario();
-			usuario.setNombre("Andres");
-			usuario.setApellido("Guzman");
-			usuario.setEmail("lalala@hotmail.com");
-			model.addAttribute("usuario", usuario);
-			model.addAttribute("titulo", "Perfil: ".concat(usuario.getNombre()));
-			return "perfil";
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Andres");
+		usuario.setApellido("Guzman");
+		usuario.setEmail("lalala@hotmail.com");
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil: ".concat(usuario.getNombre()));
+		return "perfil";
 	}
-	
+
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
 	public String listar(Model model) {
-		
+		model.addAttribute("titulo", "Listado de Usuarios");
+		return "listar";
+	}
+
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios() {
 		List<Usuario> usuarios = new ArrayList<>();
 		usuarios.add(new Usuario("Andres", "Guzman", "lalala@hotmail.com"));
 		usuarios.add(new Usuario("Jhon", "Guzman", "lalalb@hotmail.com"));
 		usuarios.add(new Usuario("Jane", "Guzman", "lalalc@hotmail.com"));
-		
-		model.addAttribute("usuarios", usuarios);
-		model.addAttribute("titulo", "Listado de Usuarios");
-		return "listar";
+		return usuarios;
 	}
-	
-	
+
 }
