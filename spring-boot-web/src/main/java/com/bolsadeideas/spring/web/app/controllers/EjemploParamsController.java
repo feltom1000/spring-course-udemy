71e145893c0077da180b9806de41125c9db26851
Tomas Molnar <tomas.molnar@ayi.group>
2022-07-13 11:51:10 -0300

    HttpServletRequest

--- spring-boot-web/src/main/java/com/bolsadeideas/spring/web/app/controllers/EjemploParamsController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/spring/web/app/controllers/EjemploParamsController.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.spring.web.app.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/params")
 public class EjemploParamsController {
-	
+
 	@GetMapping("/")
 	public String index() {
 		return "params/index";
@@ -19,6 +21,26 @@ public class EjemploParamsController {
 	public String param(@RequestParam(required = false, defaultValue = "algun valor x defecto") String texto,
 			Model model) {
 		model.addAttribute("resultado", "El texto enviado es ... " + texto);
+		return "params/ver";
+	}
+
+	@GetMapping("/mix-params")
+	public String param(@RequestParam String saludo, @RequestParam Integer numero, Model model) {
+		model.addAttribute("resultado", "El saludo enviado es ... ¡¡¡" + saludo + "!!! y el número es ¡¡¡" +numero + "!!!");
+		return "params/ver";
+	}
+	
+	@GetMapping("/mix-params-request")
+	public String param(HttpServletRequest request, Model model) {
+		String saludo = request.getParameter("saludo");
+		Integer numero = null;
+		try {
+		numero= Integer.parseInt(request.getParameter("numero"));
+		} catch (NumberFormatException e) {
+			numero = 0;
+		}
+		
+		model.addAttribute("resultado", "El saludo enviado es ... ¡¡¡" + saludo + "!!! y el número es ¡¡¡" +numero + "!!!");
 		return "params/ver";
 	}
 
